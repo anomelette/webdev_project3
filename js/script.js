6cce1a2f6b10da434b015c831ecfc290264a9022
Amelie Yen <113392462+anomelette@users.noreply.github.com>
2025-04-15 10:34:28 -0500

    moving initMap to js file

--- js/script.js
+++ js/script.js
@@ -17,28 +17,38 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+// Google Maps
+let map;
+function initMap() {
+  // Create map
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
 
-/*Google Maps Features*/
-/*Feature 1*/
-new google.maps.Marker({
-  position: {lat: -34.397, lng: 150.644},
-  map,
-  title: "Hello World!",
-});
+  // Feature 1: Marker
+  const marker = new google.maps.Marker({
+    position: { lat: -34.397, lng: 150.644 },
+    map,
+    title: "Hello World!",
+  });
 
-/*Feature 2*/
-map.setStreetView(new google.maps.StreetViewPanorama(
-  document.getElementById('map'), {
-    position: {lat: -34.397, lng: 150.644},
-    pov: { heading: 165, pitch: 0 }
-  }
-));
+  // Feature 2: Street View
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById('map'), {
+      position: { lat: -34.397, lng: 150.644 },
+      pov: { heading: 165, pitch: 0 }
+    }
+  );
+  map.setStreetView(panorama);
 
-/*Feature 3*/
-const infoWindow = new google.maps.InfoWindow({
-  content: "<h2>My Location</h2><p>This is a cool spot.</p>"
-});
-marker.addListener("click", () => {
-  infoWindow.open(map, marker);
-});
+  // Feature 3: Info Window
+  const infoWindow = new google.maps.InfoWindow({
+    content: "<h2>My Location</h2><p>This is a cool spot.</p>"
+  });
+
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
+}
 
